53c6d8d57a4131a63a254d693d1600dc12dbd735
Emmanuel-Awolu60 <emmanuelawolu8@gmail.com>
2026-04-03 21:09:32 +0100

    Updated

--- main.tf
+++ main.tf
@@ -18,3 +18,22 @@ resource "aws_internet_gateway" "main" {
     Project = var.project_name
   }
 }
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route{
+        cidir_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags ={
+        Name    = "${var.project_name}-public-rt"
+        Project = var.project_name
+    }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
